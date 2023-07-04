e13f6a71d9589296213f469ad0c760a2896e4a9f
Lexi Geller <leximoconner@yahoo.com>
2023-07-04 11:48:22 -0400

    added middleware code

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ const path = require('path');
 // Create an instance of Express.js
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static('public'));

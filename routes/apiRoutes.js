28e2d1544560d02e1a3435a882179f56fe4c8876
Lexi Geller <leximoconner@yahoo.com>
2023-07-04 11:53:01 -0400

    added apiRoutes code, commented out old code for testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,32 @@
-const router = require('express').Router();
+/*const router = require('express').Router();
 const notes = require('../db')
 
 router.use(notes);
 
-module.exports = router;
+module.exports = router;*/
+
+// GET request to retrieve all notes
+app.get('/api/notes', (req, res) => {
+    // Read the 'db.json' file and send the parsed JSON as the response
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8'));
+    res.json(notes);
+  });
+  
+  // POST request to add a new note
+  app.post('/api/notes', (req, res) => {
+    // Read the 'db.json' file to get the existing notes
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8'));
+  
+    // Generate a unique id for the new note
+    const newNote = req.body;
+    newNote.id = Date.now();
+  
+    // Add the new note to the existing notes
+    notes.push(newNote);
+  
+    // Write the updated notes to the 'db.json' file
+    fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes));
+  
+    // Send the new note as the response
+    res.json(newNote);
+  });

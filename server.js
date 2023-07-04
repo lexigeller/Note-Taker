aa672a78de5e3d2d257f7a2b335dd8489eec4662
Lexi Geller <leximoconner@yahoo.com>
2023-07-04 11:46:50 -0400

    added server.js file to create instance of Express

---
+++ server.js
@@ -0,0 +1,8 @@
+// Import necessary modules
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Create an instance of Express.js
+const app = express();
+const PORT = process.env.PORT || 3001;
